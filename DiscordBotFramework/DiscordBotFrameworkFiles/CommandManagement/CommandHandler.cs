5ebdf09bafd63f785b69d6dfb072cb3d65eb075f
Miksuu <imiksuu96@gmail.com>
2023-07-05 14:14:57 +0300

    Rework CommandHandler

--- DiscordBotFramework/DiscordBotFrameworkFiles/CommandManagement/CommandHandler.cs
+++ DiscordBotFramework/DiscordBotFrameworkFiles/CommandManagement/CommandHandler.cs
@@ -5,66 +5,61 @@ public static class CommandHandler
     // Installs the commands that are predefined in the code itself
     public async static Task InstallCommandsAsync()
     {
-        Log.WriteLine("Starting to install the commands.");
+        try
+        {
+            Log.WriteLine("Starting to install the commands.", LogLevel.DEBUG);
 
-        var client = BotReference.GetClientRef();
-        await PrepareCommands();
+            await PrepareCommands();
 
-        client.SlashCommandExecuted += SlashCommandHandler;
+            var client = BotReference.GetClientRef();
+            client.SlashCommandExecuted += SlashCommandHandler;
 
-        return;
+            Log.WriteLine("Done installing the commands.", LogLevel.DEBUG);
+        }
+        catch (Exception ex)
+        {
+            Log.WriteLine(ex.Message, LogLevel.CRITICAL);
+            throw new InvalidOperationException(ex.Message);
+        }
     }
 
     private static async Task SlashCommandHandler(SocketSlashCommand _command)
     {
         try
         {
-            Log.WriteLine("Start of SlashCommandHandler");
+            Log.WriteLine("Start of SlashCommandHandler. OptionsCount: " + _command.Data.Options.Count +
+                " by user: " + _command.User.Id, LogLevel.DEBUG);
 
             string? firstOptionString = string.Empty;
 
-            Log.WriteLine("OptionsCount: " + _command.Data.Options.Count);
-
-            if (_command.Data.Options.Count > 0)
+            var firstOption = _command.Data.Options.FirstOrDefault();
+            if (firstOption == null)
             {
-                var firstOption = _command.Data.Options.FirstOrDefault();
-                if (firstOption == null)
-                {
-                    Log.WriteLine(nameof(firstOption) + " was null! ", LogLevel.ERROR);
-                    return;
-                }
-
-                firstOptionString = firstOption.Value.ToString();
-
-                Log.WriteLine("The command " + _command.Data.Name + " had " + _command.Data.Options.Count + " options in it." +
-                    " The first command had an argument: " + firstOptionString, LogLevel.DEBUG);
-
-                // Add a for loop here to print the command arguments, if multiple later on.
-            }
-            else
-            {
-                Log.WriteLine("The command " + _command.Data.Name + " does not have any options in it.", LogLevel.DEBUG);
+                Log.WriteLine(nameof(firstOption) + " was null! ", LogLevel.ERROR);
+                return;
             }
 
+            firstOptionString = firstOption.Value.ToString();
             if (firstOptionString == null)
             {
                 Log.WriteLine("firstOptionString was null! ", LogLevel.ERROR);
                 return;
             }
 
+            Log.WriteLine("The command " + _command.Data.Name + " had " + _command.Data.Options.Count + " options in it." +
+                " The first command had an argument: " + firstOptionString, LogLevel.DEBUG);
+
             InterfaceCommand interfaceCommand = GetCommandInstance(_command.CommandName.ToUpper().ToString());
-
-            var responseTuple = await interfaceCommand.ReceiveCommandAndCheckForAdminRights(_command, firstOptionString);
-
-            if (responseTuple.serialize)
+            var response = await interfaceCommand.ReceiveCommandAndCheckForAdminRights(_command, firstOptionString);
+            if (response.serialize)
             {
                 await SerializationManager.SerializeDB();
             }
 
             await _command.RespondAsync(BotMessaging.GetMessageResponse(
-                _command.Data.Name, responseTuple.responseString, _command.Channel.Name), ephemeral: true);
+                _command.Data.Name, response.responseString, _command.Channel.Name), ephemeral: true);
 
-            Log.WriteLine("Sending and responding to the message done.");
+            Log.WriteLine("Handling slashcommand: " + firstOptionString + " by user: " + _command.User.Id + " done", LogLevel.DEBUG);
         }
         catch (Exception ex)
         {
@@ -79,38 +74,38 @@ public static class CommandHandler
         }
     }
 
-    public static async Task PrepareCommands()
+    private static async Task PrepareCommands()
     {
-        Log.WriteLine("Starting to prepare the commands.");
-
-        var commandEnumValues = Enum.GetValues(typeof(CommandName));
-        Log.WriteLine(nameof(commandEnumValues) +
-            " length: " + commandEnumValues.Length);
-
-        foreach (CommandName commandName in commandEnumValues)
+        try
         {
-            Log.WriteLine("Looping on cmd" + nameof(commandName));
+            var commandEnumValues = Enum.GetValues(typeof(CommandName));
+            Log.WriteLine("Starting to prepare the commands with count: " + commandEnumValues.Length, LogLevel.DEBUG);
 
-            try
+            foreach (CommandName commandName in commandEnumValues)
             {
-                InterfaceCommand interfaceCommand = GetCommandInstance(commandName.ToString());
+                Log.WriteLine("Looping on cmd" + nameof(commandName));
 
-                // For commands without option, need to implement it with null check
-                await interfaceCommand.AddNewCommandWithOption();
+                try
+                {
+                    InterfaceCommand interfaceCommand = GetCommandInstance(commandName.ToString());
+                    await interfaceCommand.AddNewCommandWithOption();
+                }
+                catch (Exception ex)
+                {
+                    Log.WriteLine(ex.Message, LogLevel.CRITICAL);
+                    continue;
+                }
             }
-            catch (Exception ex)
-            {
-                Log.WriteLine(ex.Message, LogLevel.CRITICAL);
-                continue;
-            }
+            Log.WriteLine("Done preparing the commands.");
         }
-
-        Log.WriteLine("Done preparing the commands.");
-
-        return;
+        catch (Exception ex)
+        {
+            Log.WriteLine(ex.Message, LogLevel.CRITICAL);
+            throw new InvalidOperationException(ex.Message);
+        }
     }
 
-    public static InterfaceCommand GetCommandInstance(string _commandName)
+    private static InterfaceCommand GetCommandInstance(string _commandName)
     {
         try
         {
