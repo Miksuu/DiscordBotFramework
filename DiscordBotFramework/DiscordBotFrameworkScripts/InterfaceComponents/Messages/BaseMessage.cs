7966c23766178879c53bbf011795e2d48a8017fe
Miksuu <imiksuu96@gmail.com>
2023-07-12 12:27:18 +0300

    Generate message footers on every embed generation

--- DiscordBotFramework/DiscordBotFrameworkScripts/InterfaceComponents/Messages/BaseMessage.cs
+++ DiscordBotFramework/DiscordBotFrameworkScripts/InterfaceComponents/Messages/BaseMessage.cs
@@ -322,7 +322,8 @@ public abstract class BaseMessage : InterfaceMessage
                 // set the title, description, and color of the embedded MessageDescription
                 embed.WithTitle(thisInterfaceMessage.MessageEmbedTitle)
                      .WithDescription(messageForGenerating)
-                     .WithColor(messageEmbedColor);
+                     .WithColor(messageEmbedColor)
+                     .WithFooter(GenerateMessageFooter());
 
                 // add a field to the embedded MessageDescription
                 //embed.AddField("Field Name", "Field Value");
@@ -380,7 +381,8 @@ public abstract class BaseMessage : InterfaceMessage
             // set the title, description, and color of the embedded MessageDescription
             embed.WithTitle(thisInterfaceMessage.MessageEmbedTitle)
                  .WithDescription(thisInterfaceMessage.MessageDescription)
-                 .WithColor(messageEmbedColor);
+                 .WithColor(messageEmbedColor)
+                 .WithFooter(GenerateMessageFooter());
 
             await channel.ModifyMessageAsync(thisInterfaceMessage.MessageId, m => m.Embed = embed.Build());
 
