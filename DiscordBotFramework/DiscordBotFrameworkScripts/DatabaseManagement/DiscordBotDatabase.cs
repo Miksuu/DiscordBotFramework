2a7eeae2024a1b5acf3455d9c84b32dd932839aa
Miksuu <imiksuu96@gmail.com>
2023-08-03 00:56:24 +0300

    Add GetApplicationName

--- DiscordBotFramework/DiscordBotFrameworkScripts/DatabaseManagement/DiscordBotDatabase.cs
+++ DiscordBotFramework/DiscordBotFrameworkScripts/DatabaseManagement/DiscordBotDatabase.cs
@@ -30,7 +30,7 @@ public class DiscordBotDatabase
     private static DiscordBotDatabase? instance;
     private static readonly object padlock = new object();
 
-    static string appName = Assembly.GetEntryAssembly()?.GetName()?.FullName;
+    static string appName = GetApplicationName();
 
     // File paths
     public static string mainAppnameDataDirectory = @"C:\" + appName + @"\Data\";
@@ -44,4 +44,20 @@ public class DiscordBotDatabase
     [DataMember] public Admins Admins = new Admins();
     [DataMember] public Categories Categories = new Categories();
     [DataMember] public EventScheduler EventScheduler = new EventScheduler();
+
+    static string GetApplicationName()
+    {
+        // Get the current assembly (the assembly where your application is defined)
+        Assembly assembly = Assembly.GetEntryAssembly();
+
+        // Get the assembly's full name, which includes the application name
+        string assemblyName = assembly?.GetName()?.FullName;
+
+        // Extract the application name from the full name
+        // The application name is the part before the first comma in the full name
+        int commaIndex = assemblyName.IndexOf(',');
+        string appName = (commaIndex > 0) ? assemblyName.Substring(0, commaIndex) : assemblyName;
+
+        return appName;
+    }
 }
