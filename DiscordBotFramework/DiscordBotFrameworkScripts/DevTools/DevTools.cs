20d1101ce3bc2765686d6cbab2ed43ecae8d53a2
Miksuu <imiksuu96@gmail.com>
2023-08-03 17:45:56 +0300

    Refactor devtools

--- DiscordBotFramework/DiscordBotFrameworkScripts/DevTools/DevTools.cs
+++ DiscordBotFramework/DiscordBotFrameworkScripts/DevTools/DevTools.cs
@@ -1,37 +1,28 @@
-﻿using Discord;
-using Discord.WebSocket;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Discord.WebSocket;
 
 public static class DevTools
 {
-    private static SocketGuild socketGuild;
-
     // !!!
     // ONLY FOR TESTING, DELETES ALL CHANNELS AND CATEGORIES
     // !!!
 
     public async static Task DeleteAllCategoriesChannelsAndRoles()
     {
-        socketGuild = BotReference.GetGuildRef();
-
         await DeleteCategories(new List<string> { "main-category" });
         await DeleteChannels(new List<string> { "info", "test", "main-category" });
 
         //FileManager.DeleteDirectoryIfItExists(Log.logsPath);
 
         await DeleteDatabase();
-
-        await DeleteRoles(new List<string> { "FrameworkTest", "Developer", "Server Booster", "DiscordBotFrameworkDev", "Discord Me", "@everyone", "@here" });
+        await DeleteRoles(new List<string> { "AirCombatMatchmakerBotDev", "FrameworkTest", "Developer", "Server Booster", "DiscordBotFrameworkDev", "Discord Me", "@everyone", "@here" });
     }
 
     private async static Task DeleteCategories(List<string> _categoriesNotToDelete)
     {
+        var guild = BotReference.GetGuildRef();
+
         Log.WriteLine("Deleting all categories with count: " + _categoriesNotToDelete.Count, LogLevel.DEBUG);
-        foreach (SocketCategoryChannel category in socketGuild.CategoryChannels)
+        foreach (SocketCategoryChannel category in guild.CategoryChannels)
         {
             Log.WriteLine("Looping on category : " + category.Name);
 
@@ -51,8 +42,10 @@ public static class DevTools
 
     private async static Task DeleteChannels(List<string> _channelsNotToDelete)
     {
+        var guild = BotReference.GetGuildRef();
+
         Log.WriteLine("Deleting all channels with count: " + _channelsNotToDelete.Count, LogLevel.DEBUG);
-        foreach (SocketGuildChannel channel in socketGuild.Channels)
+        foreach (SocketGuildChannel channel in guild.Channels)
         {
             Log.WriteLine("Looping on channel : " + channel.Name);
 
@@ -72,9 +65,11 @@ public static class DevTools
 
     private async static Task DeleteRoles(List<string> _rolesNotToDelete)
     {
+        var guild = BotReference.GetGuildRef();
+
         Log.WriteLine("Deleting all roles with count: " + _rolesNotToDelete.Count, LogLevel.DEBUG);
         // Delete roles here
-        foreach (SocketRole role in socketGuild.Roles)
+        foreach (SocketRole role in guild.Roles)
         {
             Log.WriteLine("on role: " + role.Name);
 
