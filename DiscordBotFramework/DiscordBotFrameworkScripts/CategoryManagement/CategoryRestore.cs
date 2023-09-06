a13323c1de1c6dec97e2dc8102bd22c60bcfb8e8
Miksuu <imiksuu96@gmail.com>
2023-09-06 16:46:58 +0300

    Move Channel and CategoryRestore to the framework

---
+++ DiscordBotFramework/DiscordBotFrameworkScripts/CategoryManagement/CategoryRestore.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+
+
+public static class CategoryRestore
+{
+    public static bool CheckIfLeagueCategoryHasBeenDeletedAndRestoreForCategory(
+        ulong _categoryKey, SocketGuild _guild, string _categoryName)
+    {
+        Log.WriteLine("Checking if categoryId: " + _categoryKey +
+            " has been deleted.");
+
+        if (_guild.CategoryChannels.Any(x => x.Name == _categoryName))
+        {
+            Log.WriteLine("Category found, returning. ");
+            return true;
+        }
+
+        Log.WriteLine("Category " + _categoryKey +
+            " not found, regenerating it...", LogLevel.DEBUG);
+
+        // Delete the old entry from the database
+        //DiscordBotDatabase.Instance.Categories.RemoveFromCreatedCategoryWithChannelWithKey(
+        //    _categoryKey);
+
+        return false;
+    }
+}
---
+++ DiscordBotFramework/DiscordBotFrameworkScripts/ChannelManagement/ChannelRestore.cs
@@ -0,0 +1,38 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+
+public static class ChannelRestore
+{
+    public static bool CheckIfChannelHasBeenDeletedAndRestoreForCategory(
+        ulong _categoryId,
+        InterfaceChannel _interfaceChannel)
+    {
+        var guild = BotReference.GetGuildRef();
+
+        Log.WriteLine("Checking if channel in " + _categoryId +
+            " has been deleted. Trying to find: " + _interfaceChannel.ChannelId, LogLevel.DEBUG);
+
+        if (guild.GetCategoryChannel(_categoryId).Channels.Any(
+            x => x.Id == _interfaceChannel.ChannelId))
+        {
+            Log.WriteLine("Channel found, returning. ");
+            return true;
+        }
+
+        // Handles deleting the old value
+        var dbKeyValue =
+            DiscordBotDatabase.Instance.Categories.FindInterfaceCategoryWithCategoryId(
+                _categoryId);
+
+        var dbFinal = dbKeyValue.InterfaceChannels.FirstOrDefault(
+            ic => ic.Value.ChannelId == _interfaceChannel.ChannelId);
+
+        dbKeyValue.InterfaceChannels.TryRemove(dbFinal.Value.ChannelId, out InterfaceChannel? _ic);
+
+        Log.WriteLine("Channel " + _interfaceChannel.ChannelType +
+            " not found, regenerating it...", LogLevel.ERROR);
+
+        return false;
+    }
+}   
